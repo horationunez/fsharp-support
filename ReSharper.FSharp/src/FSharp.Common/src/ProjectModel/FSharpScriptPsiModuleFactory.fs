6b5abdc4b02f3d920e8b619d757357e429b3f116
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-01-30 23:00:35 +0300

    Initial IPsiModule implementation

--- ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/FSharpScriptPsiModuleFactory.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/FSharpScriptPsiModuleFactory.fs
@@ -1,48 +1,104 @@
 namespace JetBrains.ReSharper.Plugins.FSharp.ProjectModel
 
-open System.Collections.Concurrent
+open System
 open System.Collections.Generic
 open JetBrains.Application.changes
 open JetBrains.DataFlow
+open JetBrains.DocumentManagers
 open JetBrains.ProjectModel
+open JetBrains.ReSharper.Plugins.FSharp.Common.Checker
 open JetBrains.ReSharper.Plugins.FSharp.Common.Util
 open JetBrains.ReSharper.Plugins.FSharp.ProjectModelBase
+open JetBrains.ReSharper.Plugins.FSharp.Psi
+open JetBrains.ReSharper.Psi
+open JetBrains.ReSharper.Psi.Impl
 open JetBrains.ReSharper.Psi.Modules
+open JetBrains.Threading
 open JetBrains.Util
 open JetBrains.Util.DataStructures
 
+type FSharpScriptPsiModule(file: IProjectFile, documentManager: DocumentManager,
+                           sourceFilePropertiesManager: PsiSourceFilePropertiesManager) as this =
+    inherit ConcurrentUserDataHolder()
+
+    let solution = file.GetSolution()
+    let psiServices = solution.GetPsiServices()
+
+    let checkIsValid () = file.IsValid() && psiServices.Modules.HasModule(this)
+
+    let sourceFile =
+        // todo
+        let propertiesProvider = Func<_,_,_>(fun pf sf -> sourceFilePropertiesManager.GetPsiProperties(null, pf, sf))
+        let checkIsValid = Func<_,_,_>(fun _ _ -> true)
+        let resolveContext = (this :> IPsiModule).GetResolveContextEx(file)
+        PsiProjectFile(this, file, propertiesProvider, checkIsValid, documentManager, resolveContext)
+
+    interface IPsiModule with
+        member x.Name = file.Name
+        member x.DisplayName = file.Name
+        member x.GetPersistentID() = file.Location.FullPath
+
+        member x.TargetFrameworkId = null // get highest known
+        member x.GetPsiServices() = psiServices
+        member x.GetSolution() = solution
+
+        member x.PsiLanguage = FSharpLanguage.Instance :> _
+        member x.ProjectFileType = FSharpScriptProjectFileType.Instance :> _
+
+        member x.SourceFiles = Seq.empty
+        member x.GetReferences(moduleReferenceResolveContext) = Seq.empty
+
+        member x.ContainingProjectModule = null
+        member x.GetAllDefines() = EmptyList.Instance :> _
+        member x.IsValid() = checkIsValid ()
+
+
 [<SolutionComponent>]
-type FSharpScriptPsiModuleFactory(lifetime: Lifetime, solution: ISolution, changeManager: ChangeManager) as this =
+type FSharpScriptPsiModuleFactory(lifetime, solution: ISolution, changeManager: ChangeManager, documentManager,
+                                  sourceFilePropertiesManager, fsCheckerService: FSharpCheckerService) as this =
     inherit RecursiveProjectModelChangeDeltaVisitor()
 
-    let scriptPaths = HashSet<FileSystemPath>()
+    let locker = JetFastSemiReenterableRWLock()
+    let scriptModules = Dictionary<FileSystemPath, IPsiModule>()
 
-    do changeManager.Changed2.Advise(lifetime, this.ProcessChange)
-
-    let add path = lock scriptPaths (fun _ -> scriptPaths.Add(path) |> ignore)
-    let remove path = lock scriptPaths (fun _ -> scriptPaths.Remove(path) |> ignore)
-
-    member x.ProcessChange(args: ChangeEventArgs) =
+    let processChange (args: ChangeEventArgs) =
         let change = args.ChangeMap.GetChange<ProjectModelChange>(solution)
         if isNotNull change then
-            x.VisitDelta(change)
+            use lock = locker.UsingWriteLock()
+            this.VisitDelta(change)
+
+    let createPsiModule file =
+        FSharpScriptPsiModule(file, documentManager, sourceFilePropertiesManager)
+
+    do
+        use lock = locker.UsingWriteLock()
+        for project in solution.GetAllProjects() do
+            project.GetAllProjectFiles()
+            |> List.ofSeq
+            |> List.filter (fun f -> f.LanguageType.Equals(FSharpScriptProjectFileType.Instance))
+            |> List.iter (fun f -> scriptModules.[f.Location] <- createPsiModule f)
+
+        changeManager.Changed2.Advise(lifetime, processChange)
 
     override x.VisitDelta(change: ProjectModelChange) =
+        // todo: notify anyone on add/remove?
         match change.ProjectModelElement with
         | :? IProjectFile as file when file.LanguageType.Equals(FSharpScriptProjectFileType.Instance) ->
             if change.IsAdded then
-                add file.Location
+                scriptModules.[file.Location] <- createPsiModule file
 
             if change.IsRemoved then
-                remove file.Location
+                scriptModules.Remove(file.Location) |> ignore
 
             if change.IsMovedIn || change.IsMovedOut then
-                add file.Location
+                scriptModules.[file.Location] <- createPsiModule file
                 match change with
                 | :? ProjectItemChange as itemChange ->
-                    remove itemChange.OldLocation
+                    scriptModules.Remove(itemChange.OldLocation) |> ignore
                 | _ -> ()
         | _ -> base.VisitDelta(change)
 
     interface IPsiModuleFactory with
-        member x.Modules = HybridCollection.Empty
+        member x.Modules =
+            use lock = locker.UsingReadLock()
+            HybridCollection<IPsiModule>(scriptModules.Values)
