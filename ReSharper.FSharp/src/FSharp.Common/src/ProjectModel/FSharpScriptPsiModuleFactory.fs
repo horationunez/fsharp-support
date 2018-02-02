d069ba319f4d9ff85f71e684605876b4d9b49538
Eugene Auduchinok <eugene.auduchinok@jetbrains.com>
2018-02-02 11:58:34 +0300

    tmp

--- ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/FSharpScriptPsiModuleFactory.fs
+++ ReSharper.FSharp/src/FSharp.Common/src/ProjectModel/FSharpScriptPsiModuleFactory.fs
@@ -26,17 +26,10 @@ type FSharpScriptPsiModule(file: IProjectFile, documentManager: DocumentManager,
 
     let checkIsValid () = file.IsValid() && psiServices.Modules.HasModule(this)
 
-    let sourceFile =
-        // todo
-        let propertiesProvider = Func<_,_,_>(fun pf sf -> sourceFilePropertiesManager.GetPsiProperties(null, pf, sf))
-        let checkIsValid = Func<_,_,_>(fun _ _ -> true)
-        let resolveContext = (this :> IPsiModule).GetResolveContextEx(file)
-        PsiProjectFile(this, file, propertiesProvider, checkIsValid, documentManager, resolveContext)
-
     interface IPsiModule with
         member x.Name = file.Name
         member x.DisplayName = file.Name
-        member x.GetPersistentID() = file.Location.FullPath
+        member x.GetPersistentID() = "FSharpScriptModule:" + file.Location.FullPath
 
         member x.TargetFrameworkId = null // get highest known
         member x.GetPsiServices() = psiServices
@@ -53,52 +46,68 @@ type FSharpScriptPsiModule(file: IProjectFile, documentManager: DocumentManager,
         member x.IsValid() = checkIsValid ()
 
 
+type FSharpScriptModuleHandler(handler) =
+    inherit DelegatingProjectPsiModuleHandler(handler)
+
+    override x.GetAllModules() = handler.GetAllModules() // todo: add script module here
+
+
 [<SolutionComponent>]
-type FSharpScriptPsiModuleFactory(lifetime, solution: ISolution, changeManager: ChangeManager, documentManager,
-                                  sourceFilePropertiesManager, fsCheckerService: FSharpCheckerService) as this =
-    inherit RecursiveProjectModelChangeDeltaVisitor()
+type FSharpScriptModuleFromProjectHandlerProvider() =
+    interface IProjectPsiModuleProviderFilter with
+        member x.OverrideHandler(lifetime, project, handler) =
+            FSharpScriptModuleHandler(handler) :> _, null
 
-    let locker = JetFastSemiReenterableRWLock()
-    let scriptModules = Dictionary<FileSystemPath, IPsiModule>()
+type FSharpScriptModuleFromMiscFilesProvider() =
+    class end
 
-    let processChange (args: ChangeEventArgs) =
-        let change = args.ChangeMap.GetChange<ProjectModelChange>(solution)
-        if isNotNull change then
-            use lock = locker.UsingWriteLock()
-            this.VisitDelta(change)
 
-    let createPsiModule file =
-        FSharpScriptPsiModule(file, documentManager, sourceFilePropertiesManager)
-
-    do
-        use lock = locker.UsingWriteLock()
-        for project in solution.GetAllProjects() do
-            project.GetAllProjectFiles()
-            |> List.ofSeq
-            |> List.filter (fun f -> f.LanguageType.Equals(FSharpScriptProjectFileType.Instance))
-            |> List.iter (fun f -> scriptModules.[f.Location] <- createPsiModule f)
-
-        changeManager.Changed2.Advise(lifetime, processChange)
-
-    override x.VisitDelta(change: ProjectModelChange) =
-        // todo: notify anyone on add/remove?
-        match change.ProjectModelElement with
-        | :? IProjectFile as file when file.LanguageType.Equals(FSharpScriptProjectFileType.Instance) ->
-            if change.IsAdded then
-                scriptModules.[file.Location] <- createPsiModule file
-
-            if change.IsRemoved then
-                scriptModules.Remove(file.Location) |> ignore
-
-            if change.IsMovedIn || change.IsMovedOut then
-                scriptModules.[file.Location] <- createPsiModule file
-                match change with
-                | :? ProjectItemChange as itemChange ->
-                    scriptModules.Remove(itemChange.OldLocation) |> ignore
-                | _ -> ()
-        | _ -> base.VisitDelta(change)
-
-    interface IPsiModuleFactory with
-        member x.Modules =
-            use lock = locker.UsingReadLock()
-            HybridCollection<IPsiModule>(scriptModules.Values)
+//[<SolutionComponent>]
+//type FSharpScriptPsiModuleFactory(lifetime, solution: ISolution, changeManager: ChangeManager, documentManager,
+//                                  sourceFilePropertiesManager, fsCheckerService: FSharpCheckerService) as this =
+//    inherit RecursiveProjectModelChangeDeltaVisitor()
+//
+//    let locker = JetFastSemiReenterableRWLock()
+//    let scriptModules = Dictionary<FileSystemPath, IPsiModule>()
+//
+//    let processChange (args: ChangeEventArgs) =
+//        let change = args.ChangeMap.GetChange<ProjectModelChange>(solution)
+//        if isNotNull change then
+//            use lock = locker.UsingWriteLock()
+//            this.VisitDelta(change)
+//
+//    let createPsiModule file =
+//        FSharpScriptPsiModule(file, documentManager, sourceFilePropertiesManager)
+//
+//    do
+//        use lock = locker.UsingWriteLock()
+//        for project in solution.GetAllProjects() do
+//            project.GetAllProjectFiles()
+//            |> List.ofSeq
+//            |> List.filter (fun f -> f.LanguageType.Equals(FSharpScriptProjectFileType.Instance))
+//            |> List.iter (fun f -> scriptModules.[f.Location] <- createPsiModule f)
+//
+//        changeManager.Changed2.Advise(lifetime, processChange)
+//
+//    override x.VisitDelta(change: ProjectModelChange) =
+//        // todo: notify anyone on add/remove?
+//        match change.ProjectModelElement with
+//        | :? IProjectFile as file when file.LanguageType.Equals(FSharpScriptProjectFileType.Instance) ->
+//            if change.IsAdded then
+//                scriptModules.[file.Location] <- createPsiModule file
+//
+//            if change.IsRemoved then
+//                scriptModules.Remove(file.Location) |> ignore
+//
+//            if change.IsMovedIn || change.IsMovedOut then
+//                scriptModules.[file.Location] <- createPsiModule file
+//                match change with
+//                | :? ProjectItemChange as itemChange ->
+//                    scriptModules.Remove(itemChange.OldLocation) |> ignore
+//                | _ -> ()
+//        | _ -> base.VisitDelta(change)
+//
+//    interface IPsiModuleFactory with
+//        member x.Modules =
+//            use lock = locker.UsingReadLock()
+//            HybridCollection<IPsiModule>(scriptModules.Values)
